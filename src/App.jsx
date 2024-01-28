aac0246a54634008ac0822e17adfbaf09e8a3007
Shubh <shubh2268@gmail.com>
2024-01-28 19:53:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-3xl font-bold underline'>
+      GalleryGaze
+    </div>
+  )
+}
+
+export default App
